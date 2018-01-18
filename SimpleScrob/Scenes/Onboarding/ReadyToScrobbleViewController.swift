58d908e1ea3abae0e67d4074e4dced1b40f50b6d
Josh Freed <josh@jpfreed.com>
2018-01-18 11:37:07 -0500

    After the user finished onboarding, present the main scrobble view if the user leaves the app and returns to it

--- SimpleScrob/Scenes/Onboarding/ReadyToScrobbleViewController.swift
+++ SimpleScrob/Scenes/Onboarding/ReadyToScrobbleViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import CocoaLumberjack
 
 class ReadyToScrobbleViewController: UIViewController {
     var songScanner: SongScanner!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        NotificationCenter.default.addObserver(self, selector: #selector(applicationWillEnterForeground), name: .UIApplicationWillEnterForeground, object: nil)
         
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         songScanner = appDelegate.songScanner
         songScanner.initializeSongDatabase()
+    }
+    
+    @objc func applicationWillEnterForeground() {
+        DDLogVerbose("ReadyToScrobbleViewController::applicationWillEnterForeground")
+        
+        if songScanner.isInitialized {
+            let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+            let vc = storyboard.instantiateViewController(withIdentifier: "ScrobbleViewController")
+            present(vc, animated: false) {
+                NotificationCenter.default.removeObserver(self, name: .UIApplicationWillEnterForeground, object: nil)
+            }
+        }
     }
 }
