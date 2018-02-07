fa67bb3d59d8a7e829a730024b3394fc0ca4c285
Josh Freed <josh@jpfreed.com>
2018-02-07 17:34:16 -0500

    Logs to papertrail seem to be cutting off. Made some changes to try and fix that. And, to try and log if the app is run in the background at all

--- SimpleScrob/AppDelegate.swift
+++ SimpleScrob/AppDelegate.swift
@@ -84,32 +84,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        DDLogVerbose("applicationWillResignActive")
+        DDLogDebug("applicationWillResignActive")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        DDLogVerbose("applicationDidEnterBackground")
+        DDLogDebug("applicationDidEnterBackground")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
         RMPaperTrailLogger.sharedInstance()?.disconnectAfterReadingAndWriting()
-        DDLogVerbose("applicationWillEnterForeground")
+        DDLogDebug("applicationWillEnterForeground")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        DDLogVerbose("applicationDidBecomeActive")
+        DDLogDebug("applicationDidBecomeActive")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        DDLogVerbose("applicationWillTerminate")
+        DDLogDebug("applicationWillTerminate")
     }
     
     func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        RMPaperTrailLogger.sharedInstance()?.disconnectAfterReadingAndWriting()
         DDLogInfo("Performing background refresh")
         
         let scrobbleWorker = ScrobbleWorker(
